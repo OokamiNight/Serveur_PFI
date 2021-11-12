0ab3c97d11fcf29b173145fe24f899193c32f34d
Chourot Nicolas <nicolaschourot@videotron.ca>
2021-11-12 16:45:16 -0500

    Code review

--- server.js
+++ server.js
@@ -104,6 +104,15 @@ const server = require('http').createServer(async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`HTTP Server running on port ${PORT}...`));
+server.listen(PORT, () =>{
+     console.log("**********************************");
+     console.log("* API SERVER version 2.0         *");
+     console.log("**********************************");
+     console.log("* Author: Nicolas Chourot        *");
+     console.log("* Lionel-Groulx College          *");
+     console.log("* Release date: 13 novembre 2021 *");
+     console.log("**********************************");
+     console.log(`HTTP Server running on port ${PORT}...`);
+    });
 showMemoryUsage();
 
